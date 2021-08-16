1021533b4f4b69ae1da5db8b0babd5c751d240c4
Ian Wiseman <68253726+ian-wiseman@users.noreply.github.com>
2021-08-16 14:24:15 +0200

    add quotation mark after ...Great on line 9 to fix syntax error

--- test.py
+++ test.py
@@ -6,6 +6,6 @@ from .main import app
 client = TestClient(app)
 
 def test_get_genes_by_name(pattern: str, db: Session = Depends(get_db)):
-    response = client.get("genesets/search/Great)
+    response = client.get("genesets/search/Great")
     assert.response.status_code == 200
     assert.response.json() == {"name": "Great Genes"}
