9b6f63d2ef87c0334814b342f2921f88f0cae699
Ian Wiseman <68253726+ian-wiseman@users.noreply.github.com>
2021-08-19 12:52:31 +0200

    Update output schema for search gene by name
    The response model will only return "geneset_id" attribute

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ def get_genes(db: Session = Depends(get_db)):
     return genes
         
 #new API method to retrieve gene by name
-@app.get("/genes/search/{pattern}", response_model=List[schemas.Gene])
+@app.get("/genes/search/{pattern}", response_model=List[schemas.Gene], response_model_include=["geneset_id"])
 def get_genes_by_name(pattern: str, db: Session = Depends(get_db)):
     genes = crud.get_genes_by_name(db, pattern)
     return genes
